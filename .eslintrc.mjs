bb459d317e2b704766524add587c1937ce035a5a
thepsySunny <tjsgnl2002@thepsyentist.com>
2025-03-26 23:20:48 +0900

    feat: 프리티어 설정

---
+++ .eslintrc.mjs
@@ -0,0 +1,25 @@
+export default {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:prettier/recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    'prettier/prettier': 'error',
+    'indent': ['error', 2],
+    'linebreak-style': ['error', 'unix'],
+    'quotes': ['error', 'single'],
+    'semi': ['error', 'always'],
+    'no-unused-vars': 'warn',
+    'no-console': 'warn',
+  },
+  plugins: ['prettier'],
+}; 
