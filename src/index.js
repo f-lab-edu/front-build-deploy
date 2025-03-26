23ba6b0bf0c84d6af847f64578fa069836205f2e
thepsySunny <tjsgnl2002@thepsyentist.com>
2025-03-26 22:09:09 +0900

    feat: babel및 트랜스파일러 설정 이해하기

---
+++ src/index.js
@@ -0,0 +1,12 @@
+
+const arrowFunction = () => {
+    return "Hello, Babel!";
+  };
+  
+  const promiseExample = new Promise((resolve, reject) => {
+    setTimeout(() => resolve("Promise resolved!"), 1000);
+  });
+  
+  promiseExample.then(console.log);
+  
+  console.log(arrowFunction());
